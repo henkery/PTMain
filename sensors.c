09b864f9ed1c44dd5ab092b771c7162a8ba868f9
loljimmo12 <spongebob.958@gmail.com>
2015-11-19 13:42:45 +0100

    Fixed includes for sensors

--- sensors.c
+++ sensors.c
@@ -1,5 +1,8 @@
 //This is the sensors module of the PT control system.
 #include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "header.h"
 #include "sensors.h"
 
@@ -7,22 +10,22 @@ int run;
 
 void *sns_sensor_loop(void* vd_data)
 {
-	mn_core_data *data = (mn_core_data *)vd_data;
-	int file;
-	char *filename = "/dev/i2c-1";
-	if ((file = open(filename, O_RDWR)) < 0) {
-	    /* ERROR HANDLING: you can check errno to see what went wrong */
-	    perror("Failed to open the i2c bus");
-	    return -1;
-	}
+    mn_core_data *data = (mn_core_data *)vd_data;
+    int file;
+    char *filename = "/dev/i2c-1";
+    if ((file = open(filename, O_RDWR)) < 0) {
+        /* ERROR HANDLING: you can check errno to see what went wrong */
+        //perror("Failed to open the i2c bus");
+        return 0;
+    }
 
-	while (data->run) {
+    while (data->run) {
 
-	}
-	return 0;
+    }
+    return 0;
 }
 
 int sns_sensor_run(pthread_t *thread, mn_core_data *data) {
-	pthread_create(thread, NULL, sns_sensor_loop, data);
-	return 0;
+    pthread_create(thread, NULL, sns_sensor_loop, data);
+    return 0;
 }
